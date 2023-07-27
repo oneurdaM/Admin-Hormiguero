fbe99d8a008f41a30ad999947494f6a0a955e7b3
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2023-07-27 10:40:11 -0600

    change: responsive login background

--- src/components/layout/auth-layout.tsx
+++ src/components/layout/auth-layout.tsx
@@ -31,7 +31,7 @@ export default function AuthPageLayout({
         </div>
 
         <div className="flex flex-1 items-center justify-center bg-gray-100">
-          <div className="m-auto w-full max-w-[420px] rounded bg-light p-5 sm:p-8 sm:shadow">
+          <div className="m-auto w-full max-w-[420px] rounded bg-gray-100 p-5 sm:p-8 sm:shadow md:bg-light">
             <div className="flex h-[100px] items-center justify-center">
               <Logo />
             </div>
