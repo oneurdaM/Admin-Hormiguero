50725804e2fdd9deb32802fd736f265ee0e7b058
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2023-08-07 11:08:45 -0600

    feat: log in admin

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -37,10 +37,9 @@ const LoginForm = () => {
           if (data?.jwt) {
             const role = data?.role
             if (hasAccess(allowedRoles, role)) {
-              // setAuthCredentials(data.jwt, data.role)
-              // Router.push(Routes.users.list) // redirect to "/users"
-              // return
-              console.log('not loggin')
+              setAuthCredentials(data.jwt, data.role)
+              Router.push(Routes.users.list) // redirect to "/users"
+              return
             }
             setErrorMessage('form:error-enough-permission')
           } else {
