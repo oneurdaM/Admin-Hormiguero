d56171873e7a4d67193f182f751244f1f79573f6
EduardoGarciaEnriquez <garciaenriquez.carloseduardo@gmail.com>
2023-11-06 12:21:45 -0600

    delete comented code.

--- src/components/layout/auth-layout.tsx
+++ src/components/layout/auth-layout.tsx
@@ -7,22 +7,8 @@ import logo from '../../assets/placeholders/logo-bw.png'
 export default function AuthPageLayout({
   children,
 }: React.PropsWithChildren<{}>) {
-  // const { locale } = useRouter();
-  // const dir = locale === 'ar' || locale === 'he' ? 'rtl' : 'ltr';
-
   return (
     <>
-      {/* <div // commented jul, 27th, 2023
-      className="flex h-screen items-center justify-center sm:bg-gray-100"
-      dir="ltr"
-    >
-      <div className="m-auto w-full max-w-[420px] rounded bg-light p-5 sm:p-8 sm:shadow">
-        <div className="flex h-[100px] items-center justify-center">
-          <Logo />
-        </div>
-        {children}
-      </div>
-    </div> */}
       <div className="flex h-screen flex-row" dir="ltr">
         <div className="hidden flex-1 items-center justify-center bg-dark text-white md:flex ">
           <div className=" w-auto max-w-[700px]">
